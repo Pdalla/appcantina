aaa56ae47923da85e46e05dcba5eb91b133c507c
Pdalla <83130505+Pdalla@users.noreply.github.com>
2022-10-28 15:52:13 -0300

    Update teste
    Realizei um update teste

--- src/app/cantina/entidades/Lanches.java
+++ src/app/cantina/entidades/Lanches.java
@@ -3,7 +3,7 @@ package app.cantina.entidades;
 public class Lanches extends Bebidas{
     private int oplanches;
     private int qtdelanches;
-
+    //Oi eu sou o bruno 
     public int getOplanches() {
         return oplanches;
     }
